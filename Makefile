774669ba56bae976875823348f2894d5e99b9ac3
m.shilin <m.shilin@space307.com>
2021-08-22 18:55:35 +0300

    refactored

--- Makefile
+++ Makefile
@@ -3,8 +3,7 @@ DEV_COMPOSE_FLAGS=-f deployments/docker-compose.yml -p dev
 
 env_up: env_down
 	docker-compose $(DEV_COMPOSE_FLAGS) pull
-	docker-compose $(DEV_COMPOSE_FLAGS) build ${BUILD_ARGS} $(NAME)
-	docker-compose $(DEV_COMPOSE_FLAGS) up -d $(NAME)
+	docker-compose $(DEV_COMPOSE_FLAGS) up --build -d $(NAME)
 
 env_down:
 	docker-compose $(DEV_COMPOSE_FLAGS) down --volumes
--- main.go
+++ main.go
@@ -1,43 +1,46 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	_ "github.com/lib/pq"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
-
-	//http.HandleFunc("/docker", func(w http.ResponseWriter, req *http.Request) {
-	//	fmt.Fprintf(w, "<h1>Hello, World(From Docker)!")
-	//})
-	//
-	//http.ListenAndServe(":8080", nil)
+	c := make(chan os.Signal, 0)
+	signal.Notify(c)
 
 	fmt.Println("connecting")
-	//// these details match the docker-compose.yml file.
-	//postgresInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-	//	"postgres", 5432, "romax", "mypassword", "romax")
-	//db, err := sql.Open("postgres", postgresInfo)
-	//if err != nil {
-	//	panic(err)
-	//}
-	////defer db.Close()
-	//
-	//start := time.Now()
-	//for db.Ping() != nil {
-	//	if start.After(start.Add(3 * time.Second)) {
-	//		fmt.Println()
-	//		panic("failed to connect after 3 secs.")
-	//	}
-	//}
-	//fmt.Println("connected:", db.Ping() == nil)
-	//_, err = db.Exec(`DROP TABLE IF EXISTS COMPANY;`)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//_, err = db.Exec(`CREATE TABLE COMPANY (ID INT PRIMARY KEY NOT NULL, NAME text);`)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//fmt.Println("table company is created")
+	// these details match the docker-compose.yml file.
+	postgresInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		"postgres", 5432, "romax", "mypassword", "romax")
+	db, err := sql.Open("postgres", postgresInfo)
+	if err != nil {
+		panic(err)
+	}
+	//defer db.Close()
+
+	start := time.Now()
+	for db.Ping() != nil {
+		if start.After(start.Add(3 * time.Second)) {
+			fmt.Println()
+			panic("failed to connect after 3 secs.")
+		}
+	}
+	fmt.Println("connected:", db.Ping() == nil)
+	_, err = db.Exec(`DROP TABLE IF EXISTS COMPANY;`)
+	if err != nil {
+		panic(err)
+	}
+	_, err = db.Exec(`CREATE TABLE COMPANY (ID INT PRIMARY KEY NOT NULL, NAME text);`)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("table company is created")
+	fmt.Println("app started")
+	s := <-c
+	fmt.Println("Got signal:", s) //Got signal: terminated
 }
